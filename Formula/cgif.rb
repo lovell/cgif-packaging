929bf9e82b851e7dcb25f3be39bf25320ae22b57
Lovell Fuller <github@lovell.info>
2021-10-07 14:11:33 +0100

    homebrew: upgrade to version 0.0.1

--- Formula/cgif.rb
+++ Formula/cgif.rb
@@ -1,9 +1,9 @@
 class Cgif < Formula
   desc "A fast and lightweight GIF encoder"
   homepage "https://github.com/dloebl/cgif"
-  url "https://github.com/dloebl/cgif/archive/e698ff1.tar.gz"
-  version "0.0.0"
-  sha256 "4130d70311102c1a1b1aa440d3d2cbff26fb5f9e48e7e7275ec50efd2e0bd6c0"
+  url "https://github.com/dloebl/cgif/archive/V0.0.1.tar.gz"
+  version "0.0.1"
+  sha256 "4e61d6bd387182f5e0090de49197084a8676e9f5aeac0d61562f52c553d71598"
   license "MIT"
 
   bottle :unneeded
